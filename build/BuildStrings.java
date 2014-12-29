3986858289f151b49c9a66d7bf5ec606f4447b65
nathan <nathan@dorkbox.com>
2014-12-30 00:07:54 +0100

    Updated build instructions

--- build/BuildStrings.java
+++ build/BuildStrings.java
@@ -13,10 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-import java.util.ArrayList;
-
 import dorkbox.Build;
-import dorkbox.license.License;
 import dorkbox.util.FileUtil;
 
 public class BuildStrings {
@@ -31,34 +28,7 @@ public class BuildStrings {
         public static final String GitHub = Build.path("..", "..", "github_projects");
     }
 
-    public static class JavaBuilder {
-        public static final String name = "OAK";
-        public static final String root = path(BuildStrings.ProjectPath.GitHub, name);
-        public static final String src = path(root, "src");
-        public static ArrayList<License> license = new ArrayList<License>() {
-            private static final long serialVersionUID = 1L;
-            {
-                add(Licenses.OAK.OAK);
-                add(Licenses.OAK.Scar);
-                add(Licenses.DorkboxUtil.DorkboxUtil);
-                add(Licenses.DorkboxUtil.MigBase64);
-                add(Licenses.DorkboxUtil.FilenameUtils);
-                add(Licenses.DorkboxUtil.AnnotationDetector);
-
-                add(LicenseLibs.BouncyCastle);
-                add(LicenseLibs.FastMD5);
-                add(LicenseLibs.LzmaJava);
-                add(LicenseLibs.SLF4J);
-
-                add(LicenseLibs.JavaTar);
-                add(LicenseLibs.WildCard);
-                add(LicenseLibs.OpenJDK);
-                add(LicenseLibs.YamlBeans);
-            }
-        };
-    }
-
-    private static String path(String... path) {
+    static String path(String... path) {
         return FileUtil.normalizeAsFile(Build.path(path));
     }
 }
