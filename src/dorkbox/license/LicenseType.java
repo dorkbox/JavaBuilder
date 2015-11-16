1fc7460cbb68d546e878f25f78074e5a7862e6cb
nathan <nathan@dorkbox.com>
2015-11-16 22:27:00 +0100

    Added license URL location (we do this in addition to including the license as well)

--- src/dorkbox/license/LicenseType.java
+++ src/dorkbox/license/LicenseType.java
@@ -16,27 +16,29 @@
 package dorkbox.license;
 
 public enum LicenseType {
-    APACHE("Apachev2", "Apache 2.0 License"),
-    BSD2("BSD2", "BSD 2-clause License"), // same as New BSD
-    BSD3("BSD3", "BSD 3-clause License"),
-    CC0("CC0", "CC0 License"),
-    EPL("EPLv1", "Eclipse Public License"),
-    GPLv2_CP("GPLv2_CP", "GPL v2 License, with Classpath exception"),
-    LGPLv2("LGPLv2.1", "LGPL v2.1 License"),
-    LGPLv3("LGPLv3", "LGPL v3 License"),
-    MIT("MIT", "MIT License"), // same as MIT X11
-    MPL("MPLv1.1", "Mozilla Public License 1.1"),
-    MPL2("MPLv2.0", "Mozilla Public License 2.0"),
-    OFL("OFLv1.1", "SIL Open Font License"),
-    PUBLIC("Public", "Public Domain"),  // not quite the same as CC0 (CC0 is better)
+    APACHE("Apachev2", "Apache 2.0 License", "http://opensource.org/licenses/Apache-2.0"),
+    BSD2("BSD2", "BSD 2-clause License", "http://opensource.org/licenses/BSD-2-Clause"), // Same as "Simplified" or "FreeBSD" license
+    BSD3("BSD3", "BSD 3-clause License", "http://opensource.org/licenses/BSD-3-Clause"), // same as 'New' or 'Revised' license
+    CC0("CC0", "CC0 License", "https://creativecommons.org/publicdomain/zero/1.0/"),
+    EPL("EPLv1", "Eclipse Public License", "http://opensource.org/licenses/EPL-1.0"),
+    GPLv2_CP("GPLv2_CP", "GPL v2 License, with Classpath exception", "https://www.gnu.org/software/classpath/license.html"),
+    LGPLv2("LGPLv2.1", "LGPL v2.1 License", "http://opensource.org/licenses/LGPL-2.1"),
+    LGPLv3("LGPLv3", "LGPL v3 License", "http://opensource.org/licenses/LGPL-3.0"),
+    MIT("MIT", "MIT License", "http://www.opensource.org/licenses/mit-license.php"), // same as MIT X11
+    MPL("MPLv1.1", "Mozilla Public License 1.1", "http://opensource.org/licenses/MPL-1.1"), // replaced by MPL2.0
+    MPL2("MPLv2.0", "Mozilla Public License 2.0", "http://opensource.org/licenses/MPL-2.0"),
+    OFL("OFLv1.1", "SIL Open Font License", "http://opensource.org/licenses/OFL-1.1"),
+    PUBLIC("Public", "Public Domain", ""),  // not quite the same as CC0 (CC0 is better)
     ;
 
     private final String name;
     private final String description;
+    private final String url;
 
-    LicenseType(String name, String description) {
+    LicenseType(String name, String description, String url) {
         this.name = name;
         this.description = description;
+        this.url = url;
     }
 
     /** This is the file name extension for the license file */
@@ -46,5 +48,10 @@ public enum LicenseType {
 
     public String getDescription() {
         return this.description;
+    }
+
+    public
+    String getUrl() {
+        return url;
     }
 }
